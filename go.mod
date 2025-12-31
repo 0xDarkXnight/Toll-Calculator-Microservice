d2c9a9adab12a02585a1430ee8eeac0132dadf5b
0xDarkXnight <akshat_g1@me.iitr.ac.in>
2025-12-31 22:02:04 +0530

    OBU Init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/0xDarkXnight/Toll-Calculator-Microservice
+
+go 1.25.5
+
+require github.com/gorilla/websocket v1.5.3 // indirect
---
+++ obu/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const wsEndpoint = "ws://127.0.0.1:30000/ws"
+
+var sendInterval = time.Second
+
+type OBUData struct {
+	OBUID int     `json:"obuID"`
+	Lat   float64 `json:"lat"`
+	Long  float64 `json:"long"`
+}
+
+func genLatLong() (float64, float64) {
+	return genCoords(), genCoords()
+}
+
+func genCoords() float64 {
+	n := float64(rand.Intn(100) + 1)
+	f := rand.Float64()
+	return n + f
+}
+
+func generateOBUIDs(n int) []int {
+	ids := make([]int, n)
+	for i := 0; i < n; i++ {
+		ids[i] = rand.Intn(math.MaxInt)
+	}
+	return ids
+}
+
+func main() {
+	obuIDS := generateOBUIDs(20)
+	conn, _, err := websocket.DefaultDialer.Dial(wsEndpoint, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		for i := 0; i < len(obuIDS); i++ {
+			lat, long := genLatLong()
+			data := OBUData{
+				OBUID: obuIDS[i],
+				Lat:   lat,
+				Long:  long,
+			}
+			if err := conn.WriteJSON(data); err != nil {
+				log.Fatal(err)
+			}
+		}
+		fmt.Println(genCoords())
+		time.Sleep(sendInterval)
+	}
+}
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
